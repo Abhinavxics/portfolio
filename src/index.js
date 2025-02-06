1f0f1435f53154e42469ff25361dcfc5b9dbd575
Abhinavxics <abhinavcodesleep.com@gmail.com>
2025-02-06 21:02:51 +0530

    Fix: Remove router and update dependencies

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter basename="/portfolio">
-        <App />
-      </BrowserRouter>
+      <App />
     </ErrorBoundary>
   </React.StrictMode>
 ); 
